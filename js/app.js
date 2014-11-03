366a1fbc81c0280e7fad6960ec2f48d3f67fb7ca
basiacoulter <hello@basiacoulterdesign.com>
2014-11-03 17:14:58 -0500

    toggle

--- js/app.js
+++ js/app.js
@@ -9,15 +9,11 @@ $(document).ready(function() {
 		if (value.length > 0) {
 			$(".list").prepend(listItem);
 		} 
-		$(".fa-check-square-o").on("click", function() {
-			if ($(this).closest("div").find("p").hasClass("checked")) {
-				$(this).closest("div").find("p").removeClass("checked");
-			} else {
-			$(this).closest("div").find("p").addClass("checked");
-			}
+	});
+	$(".fa-check-square-o").on("click", function() {
+			$(this).closest("div").find("p").toggleClass("checked");
 		});
-		$(".fa-minus-square-o").on("click", function() {
+	$(".fa-minus-square-o").on("click", function() {
 			$(this).closest("div").remove();
 		});
-	});
 })
